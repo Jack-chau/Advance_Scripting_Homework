7d2155efecbe6f6c13b20713d044e357bc8e4803
I am Jack <74429355+Jack-chau@users.noreply.github.com>
2024-11-21 23:42:57 +0800

    tkinter first tutorial

---
+++ tkinter/grid.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title( 'simple app' )
+
+def on_click() :
+    label.config( text="Button Clicked!")
+
+label = tk.Label( root, text = "Label 1" )
+label.grid( row=0, column=0 )
+
+print( label.config().keys() )
+
+btn = tk.Button( root, text = "Button 1", command = on_click )
+btn.grid( row=0, column=1 )
+
+
+root.mainloop()
