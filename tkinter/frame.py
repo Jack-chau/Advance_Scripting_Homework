0d3a232ca0d6583c3331d750dd163d6d1dce3bdc
I am Jack <74429355+Jack-chau@users.noreply.github.com>
2024-11-22 15:15:29 +0800

    tkinter frame

--- tkinter/frame.py
+++ tkinter/frame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 root.title( "Simple App" )
@@ -10,23 +11,40 @@ def add_to_list( ):
         entry.delete( 0, tk.END )
 
 root.columnconfigure( 0, weight=1 )
+root.columnconfigure( 1, weight=2 )
 root.rowconfigure( 0, weight=1 )
 
-frame = tk.Frame( root )
+frame = ttk.Frame( root )
 frame.grid( row=0, column=0, sticky="nsew" )
 
 frame.columnconfigure( 0, weight=1 )
 frame.rowconfigure( 1, weight=1 )
 
-entry = tk.Entry( frame )
+entry = ttk.Entry( frame )
 entry.grid( row=0, column=0, sticky="ew" )
 
 entry.bind( "<Return>", lambda event: add_to_list() )
 
-entry_btn = tk.Button( frame ,text="Add", command=add_to_list )
+entry_btn = ttk.Button( frame ,text="Add", command=add_to_list )
 entry_btn.grid( row=0, column=1)
 
 text_list = tk.Listbox( frame )
 text_list.grid( row=1, column=0, columnspan=2, sticky="nsew" )
 
+frame2 = tk.Frame( root )
+frame2.grid( row=0, column=1, sticky="nsew" )
+
+frame2.columnconfigure( 0, weight=1 )
+frame2.rowconfigure( 1, weight=1 )
+
+entry = tk.Entry( frame2 )
+entry.grid( row=0, column=0, sticky="ew" )
+
+entry.bind( "<Return>", lambda event: add_to_list() )
+
+entry_btn = tk.Button( frame2 ,text="Add", command=add_to_list )
+entry_btn.grid( row=0, column=1)
+
+text_list = tk.Listbox( frame2 )
+text_list.grid( row=1, column=0, columnspan=2, sticky="nsew" )
 root.mainloop()
